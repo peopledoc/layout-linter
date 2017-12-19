5ed26667c7eb704ebd35860d0152802fadbc48b4
Edouard Lopez <contact@edouard-lopez.com>
2017-12-19 20:55:33 +0100

    remove useless tag content from tests

--- rules/has-attributes/has-attributes.test.js
+++ rules/has-attributes/has-attributes.test.js
@@ -3,17 +3,16 @@ import { JSDOM } from 'jsdom';
 import rule from './has-attributes.js';
 
 test('should find single attribute when present', (t) => {
-  const document = new JSDOM("<p class='my-class'>Hello world</p>").window;
+  const document = new JSDOM("<b class='my-class'></b>").window;
   t.true(rule('.my-class', document));
 });
 
 test('should fail when single attribute missing', (t) => {
-  const document = new JSDOM("<p class=''>Hello world</p>").window;
+  const document = new JSDOM("<b class=''></b>").window;
   t.false(rule('.my-class', document));
 });
 
 test('should find multiple attributes when present', (t) => {
-  const document = new JSDOM("<p class='my-class other-class'>Hello world</p>")
-    .window;
+  const document = new JSDOM("<b class='my-class other-class'></b>").window;
   t.true(rule('.my-class, .other-class', document));
 });
