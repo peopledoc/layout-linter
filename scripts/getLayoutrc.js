60dc20cd053ec97dc5784393ef7a121ced1b99b9
Pixelik <Pixelik@users.noreply.github.com>
2018-05-18 18:20:56 +0300

    Update getLayoutrc.js

--- scripts/getLayoutrc.js
+++ scripts/getLayoutrc.js
@@ -1,6 +1,6 @@
 /*
   - optional parameter passed is the absolute (or relative) path to a file containing the rules (json)
-  - if omitted, it will look for .layoutrc file (json) inside the app directory and alf its subdirectories
+  - if omitted, it will look for .layoutrc file (json) inside the app directory and all of its subdirectories
   - converts the json structure found inside the file into a js object and returns it
 */
 
