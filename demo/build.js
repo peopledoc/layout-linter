38d057f59f243aee183cd5be6d54bc94eda8babd
Pixelik <Pixelik@users.noreply.github.com>
2018-08-23 14:56:48 +0300

    Update build.js

--- demo/build.js
+++ demo/build.js
@@ -7,36 +7,7 @@ if (fse.existsSync(pathToIndex)) {
   fse.unlinkSync(pathToIndex);
 }
 
-let result = lintLayout({
-  /*
-    - optional
-    - absolute (or relative) path to json file containing rules
-    - will look for .layoutrc file inside app directory (and all subdirectories) if omitted
-  */
-  //layoutrc: '/some/custom/rules.config',
-
-  /*
-    - optional
-    - absolute (or relative) path to custom layout-linter .css file
-    - can alse be set to false
-    - will use default layout-linter .css if omitted, unless set to false
-  */
-  //css: '/some/custom.css',
-
-  /*
-    - optional
-    - the module always returns a complete HTML document, whether it is passed a snippet or a complete HTML document
-    - if this property is set to true, the module will return the linted snippet and not a complete HTML document
-  */
-  //snippet: true,
-
-  /*
-    - mandatory
-    - absolute (or relative) path to .html file
-    - or valid HTML string (e.g. "<div>....</div>" or "<html>....</html>" etc..)
-  */
-  source: './source.html'
-});
+let result = lintLayout({ source: './source.html' });
 
 console.log('\n\n\n');
 console.log('========= LAYOUT-LINTER | DEMO (start) =========');
